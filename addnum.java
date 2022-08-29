c72c3f26a0fcac9f0ee0418ed3540ae029c747fb
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2022-08-30 00:53:59 +0530

    Add files via upload

---
+++ addnum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class addnum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter marks of first");
+        int a = sc.nextInt();
+        System.out.println("enter marks of second");
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.print(c);
+
+
+    }
+    
+}
